fbecfdff2a56404fb112a0a8cff526fa36a04127
Deep Das <deepdblm@gmail.com>
2024-08-21 21:26:16 +0530

    added navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './styles.css';
 import BudgetTrackerNew from './components/BudgetTrackerNew';
+import Navbar from './components/Navbar'; 
 
 const App: React.FC = () => {
   return (
     <div className="App">
+      <Navbar /> 
       <main>
         <BudgetTrackerNew />
       </main>
